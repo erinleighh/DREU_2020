d14d538b40e719d68e2a3382fc72a4fd215c29d8
Erin Howard <erinleighh02@gmail.com>
2020-06-18 13:20:55 -0700

    Updated flux type and normalized it, added quality check. Updated labels.

--- readFits.py
+++ readFits.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import altair as alt
 import glob
 import os
+import pandas as pd
 
 numTab = 11  # We have 11 fits files to read from.
 lightCurves = [0] * numTab  # Store the light curves for all the tables.
@@ -11,15 +12,16 @@ path = "start_data"  # Hack for dealing with OS forward/back slash conflicts.
 i = 0
 
 for file in glob.glob(os.path.join(path, "*.fits")):
-    print("\nReading in...")
+    print("\nReading in " + str(file))
     fitsTable = fits.open(file, memmap=True)
-    fitsTable.info()
-    curveData = Table(fitsTable[1].data)
+    curveTable = Table(fitsTable[1].data).to_pandas()
+    curveData = curveTable[curveTable['QUALITY'] == 0]
+    curveData['PDCSAP_FLUX']=curveData['PDCSAP_FLUX']/curveData['PDCSAP_FLUX'].median()
 
     # matplotlib graphing
-    plt.scatter(curveData['TIME'], curveData['SAP_FLUX'], color='tab:purple', s=.5)
-    plt.xlabel('Time (days)')
-    plt.ylabel('Flux')
+    plt.scatter(curveData['TIME'], curveData['PDCSAP_FLUX'], color='tab:purple', s=.5)
+    plt.xlabel('BJD - 2457000 (days)')  # BJD Julian corrected for elliptical orbit.
+    plt.ylabel('Relative Flux')
     plt.title('Light Curve')
 
     i += 1
@@ -30,8 +32,8 @@ for file in glob.glob(os.path.join(path, "*.fits")):
     # Altair graphing that doesn't work yet
     # chart = alt.Chart(curve).mark_circle(size=1).encode(
     #     x='TIME',
-    #     y='SAP_FLUX',
-    #     tooltip=['TIME', 'SAP_FLUX']
+    #     y='PDCSAP_FLUX',
+    #     tooltip=['TIME', 'PDCSAP_FLUX']
     # ).interactive()
     #
     # i += 1
